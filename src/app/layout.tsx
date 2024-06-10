44a256153d80b23492d1d53bad4b1aebb5014732
Swatantra Sinha <swatantrasinha15aug@gmail.com>
2024-06-10 13:32:46 +0530

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-
-        {children}
+        <div className="container mx-auto px-12">
+          {children}
+        </div>
       </body>
     </html>
   );
